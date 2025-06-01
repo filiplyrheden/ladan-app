687f6c908a23eb0618baadc399e6d5923bce1120
filiplyrheden <filip.lyrheden@gmail.com>
2025-06-01 15:12:46 +0200

    Added types.ts and removed unused components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import "./App.css";
-import Button from "./components/Button";
-import TextInput from "./components/TextInput";
 
 function App() {
   return (
     <>
       <h1>Ladan</h1>
-      <TextInput></TextInput>
-      <Button>Lägg till</Button>
     </>
   );
 }
